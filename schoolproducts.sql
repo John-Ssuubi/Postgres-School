523cd903206e315685568f0bc59d04e3194739e7
John-Ssuubi <91393766+John-Ssuubi@users.noreply.github.com>
2026-03-23 12:35:09 +0300

    Add files via upload

--- schoolproducts.sql
+++ schoolproducts.sql
@@ -11,5 +11,26 @@ INSERT INTO products(product, price, delivery_date) values
 ('Wireless Keyboard', 70000, '2026/04/20'),
 ('Monitor', 500000, '2026/04/22');
 
+UPDATE products SET discount='30%' 	WHERE s_no=1;
+UPDATE products SET discount='25%' 	WHERE s_no=2;
+UPDATE products SET delivery_date='2026/04/18' 	WHERE s_no=3;
+UPDATE products SET discount='30%' 	WHERE s_no=4;
+UPDATE products SET discount='40%' 	WHERE s_no=5;
+UPDATE products SET discount='30%' 	WHERE s_no=6;
+UPDATE products SET discount='25%' 	WHERE s_no=7;
+UPDATE products SET discount='10%' 	WHERE s_no=8;
+
+
+ALTER TABLE products
+ADD COLUMN discount TEXT;
+
+ALTER TABLE products
+DROP COLUMN discountamount;
 
 SELECT * FROM products;
+
+SELECT product, price, delivery_date FROM products; 
+
+DELETE FROM products WHERE s_no = 4;
+
+SELECT 0.3*price as DiscountedAmount from products;
